d492119a7c5af5d73c4785d97e23aa0e7cc6560a
irafayabdul <irafayabdul@gmail.com>
2025-06-20 23:26:28 +0200

    Implementation untill comsuming Endpoint

--- Exercise_starter_files/benchmark.sh
+++ Exercise_starter_files/benchmark.sh
@@ -70,4 +70,4 @@
 # Total:        114  160  28.0    172     199#
 
 
- ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer REPLACE_WITH_KEY' http://REPLACE_WITH_API_URL/score
+ ab -n 10 -v 4 -p data.json -T 'application/json' -H 'Authorization: Bearer 4N3KxgrZz8LpqbZDalfj5tMal8xsead9' http://d7f0cd13-b81c-41ca-a956-cf13225fee59.eastus2.azurecontainer.io/score
--- Exercise_starter_files/endpoint.py
+++ Exercise_starter_files/endpoint.py
@@ -3,31 +3,67 @@ import json
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
-scoring_uri = ""
+scoring_uri = "http://d7f0cd13-b81c-41ca-a956-cf13225fee59.eastus2.azurecontainer.io/score"
 
 # If the service is authenticated, set the key or token
-key = ""
+key = "4N3KxgrZz8LpqbZDalfj5tMal8xsead9"
 
 # Two sets of data to score, so we get two results back
+
 data = {
-    "data": [
-        {
-            "instant": 1,
-            "date": "2013-01-01 00:00:00,000000",
-            "season": 1,
-            "yr": 0,
-            "mnth": 1,
-            "weekday": 6,
-            "weathersit": 2,
-            "temp": 0.344167,
-            "atemp": 0.363625,
-            "hum": 0.805833,
-            "windspeed": 0.160446,
-            "casual": 331,
-            "registered": 654,
-        },
-    ]
+    "Inputs": {
+        "data": [
+            {
+                "age": 40,
+                "job": "blue-collar",
+                "marital": "single",
+                "education": "university.degree",
+                "default": "no",
+                "housing": "no",
+                "loan": "no",
+                "contact": "cellular",
+                "month": "aug",
+                "day_of_week": "wed",
+                "duration": 999,
+                "campaign": 1,
+                "pdays": 999,
+                "previous": 0,
+                "poutcome": "success",
+                "emp.var.rate": 1.4,
+                "cons.price.idx": 93.444,
+                "cons.conf.idx": -83.1,
+                "euribor3m": 4.9639999999999995,
+                "nr.employed": 5228.1
+            },
+            {
+                "age": 0,
+                "job": "example_value",
+                "marital": "example_value",
+                "education": "example_value",
+                "default": "example_value",
+                "housing": "example_value",
+                "loan": "example_value",
+                "contact": "example_value",
+                "month": "example_value",
+                "day_of_week": "example_value",
+                "duration": 0,
+                "campaign": 0,
+                "pdays": 0,
+                "previous": 0,
+                "poutcome": "example_value",
+                "emp.var.rate": 0,
+                "cons.price.idx": 0,
+                "cons.conf.idx": 0,
+                "euribor3m": 0,
+                "nr.employed": 0
+            }
+        ]
+    },
+    "GlobalParameters": {
+        "method": "predict"
+    }
 }
+
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
